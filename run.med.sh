b19e99d1d4669290a56b078bbfb749a31466238e
suhailbarot <suhailsbarot@gmail.com>
2019-02-08 22:33:38 -0500

    Readme added

--- run.med.sh
+++ run.med.sh
@@ -11,24 +11,24 @@
 map_path=/home/ubuntu/tools/mAP
 export PATH=$map_path:$PATH
 
-# echo "#####################################"
-# echo "#       MED with MFCC Features      #"
-# echo "#####################################"
-# mkdir -p mfcc_pred
-# # iterate over the events
-# feat_dim_mfcc=400
-# cat "list/train" "list/val" >"./merged_train"
-# for event in P001 P002 P003; do
-#   echo "=========  Event $event  ========="
-#   # now train a svm model
+echo "#####################################"
+echo "#       MED with MFCC Features      #"
+echo "#####################################"
+mkdir -p mfcc_pred
+# iterate over the events
+feat_dim_mfcc=400
+cat "list/train" "list/val" >"./merged_train"
+for event in P001 P002 P003; do
+  echo "=========  Event $event  ========="
+  # now train a svm model
 
-#   python scripts/train_svm.py $event "kmeans/" $feat_dim_mfcc mfcc_pred/svm.$event.model || exit 1;
-#   # apply the svm model to *ALL* the testing videos;
-#   # output the score of each testing video to a file ${event}_pred 
-#   python scripts/test_svm.py mfcc_pred/svm.$event.model "kmeans/" $feat_dim_mfcc mfcc_pred/${event}_mfcc.lst $event || exit 1;
-# #   # compute the average precision by calling the mAP package
-#   ap list/${event}_val_label mfcc_pred/${event}_mfcc.lst
-# done
+  python scripts/train_svm.py $event "kmeans/" $feat_dim_mfcc mfcc_pred/svm.$event.model || exit 1;
+  # apply the svm model to *ALL* the testing videos;
+  # output the score of each testing video to a file ${event}_pred 
+  python scripts/test_svm.py mfcc_pred/svm.$event.model "kmeans/" $feat_dim_mfcc mfcc_pred/${event}_mfcc.lst $event || exit 1;
+#   # compute the average precision by calling the mAP package
+  ap list/${event}_val_label mfcc_pred/${event}_mfcc.lst
+done
 
 # echo ""
 echo "#####################################"
